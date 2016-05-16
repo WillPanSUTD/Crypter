d3cc5e17b54bd960d85086f3e3f35153e2032a92
Will Pan <vpan@foxmail.com>
2016-05-16 22:59:34 +0800

    Stage 1
    Finish four parts with three of the following functions:
    1) Make a random section of data in binary (using hashing if its large!)
    2) Take in a file and convert to binary
    3) Do XOR between file and random data to encrypt/decrypt file.

--- Will/ReadNXOR.cpp
+++ Will/ReadNXOR.cpp
@@ -9,10 +9,11 @@ using namespace std;
 ////////////////////////It's a bitset template class I downloaded from http://codereview.stackexchange.com/questions/67012/easy-bitset-i-o, which could make coding easier I guess. 
 ///////////////////////Antoher alternative is http://codereview.stackexchange.com/questions/67350/easy-bit-io-very-simple-interface-works-up-to-n-64, I didn't use, you make take a try.
 //////////////////////////If you feel hard to read this, jump to line 111. Or search "int main()".
-template <std::size_t N>
+
+template <std::size_t N>		//////Please google template definition, which enables the possibility of a class with multiple types. Here, BitIo<8>, BitIo<16>, ... etc. 	
 class BitIo
 {
-public:
+public:							////Only functions and varibles defined as public can be called.
 
 	void push_back(const std::bitset<N>& bs)
 	{
@@ -115,7 +116,7 @@ int main()
 		filename2 = "f:\\sutd\\research\\prototypingproject\\programming\\result\\cc\\in",			//declare the directory of input message file. 
 		filename3 = "f:\\sutd\\research\\prototypingproject\\programming\\result\\cc\\out";			//declare the directory of output XOR result file.
 
-	BitIo<16> bio;				///the class I defiend above, to make coding with bitset variable easier
+	BitIo<16> bio;				///the class I defined above, to make coding with bitset variable easier
 
 
 	/////////////////////PART  ONE//////////////////////////////////////////////////////////////
@@ -129,22 +130,26 @@ int main()
 		info.seekg(0, info.end);////seekg is used here to make the file pointer go to the end of the file, so we can use tellg to tell the length of the file
 		length = info.tellg();
 		info.seekg(0, info.beg);////restore the pointer's position to the beginning of the file
-		message = new char(length);	///so we get the length of the message
+		message = new char[length];	///so we get the length of the message
 		int i = 0;
 		while (info.get(c))			//////get a char from info, and save it to c
 			message[i++] = c;		//////c->message
 	}
-	else cout << "unable to open info file!";
+	else
+	{
+		cout << "unable to open info file!";
+		return 0;
+	}
 	info.close();
 
 	/////////////////////////////PART TWO//////////////////////////////////////////////
 	//////////////////////////////generate the key/////////////////////////////////////////////////////
-	ofstream output(filename1, ios::out | ios::binary);											//declare the output file to store the key
+	ofstream output(filename1, ios::trunc | ios::binary);											//declare the output file to store the key
 	int a;										//an integer to get the random number
 	int N = length;								//the size of key file
 
 	//	std::hash<std::bitset<16>> hash_fn;			//hash function for bitset, seems to generate 10 digits from a bitset<16> variable, I'm not so familiar with this. Can ignore if no need.
-											
+
 	for (int i = 0; i < N; i++)		//generate N keys 
 	{
 		a = rand();					//a is from  0 to RAND_MAX, here Rand_max is 32767
@@ -161,28 +166,59 @@ int main()
 		cout << "error!" << endl;
 	output.close();
 
+	cout << bio.size() << endl;
+	//bio.clear();
 
-	//////////////////////////////PART THREE (OPTIONAL)///////////////////////////////////////////////////////////////////
-	//////////////////////////////Read the key from the file we stored/////////////////////////////////////////
-	ifstream key(filename1, ios::in | ios::binary);			///declare an input file pointer to file key
-	BitIo<16> bio2;				
 
-	if (key.is_open())
-	{
-		key >> bio2;									/////read from key file, save in bio2 vector
-		while (!bio2.empty()) {
-			cout << bio2.pop_front() << endl;			// Prints the all the keys (16-bit bitsets) in correct order.
-		}
-	}
-	else cout << "unable to open key file!";
+	////////////////////////////////PART THREE (OPTIONAL)///////////////////////////////////////////////////////////////////
+	////////////////////////////////Read the key from the file we stored/////////////////////////////////////////
+	//fstream key(filename1, ifstream::in | ifstream::binary);			///declare an input file pointer to file key
+	//BitIo<16> bio2;				
+
+	//if (key.is_open())
+	//{
+	//	key >> bio2;									/////read from key file, save in bio2 vector
+	//	while (!bio2.empty()) {
+	//		cout << bio2.pop_front() << endl;			// Prints the all the keys (16-bit bitsets) in correct order.
+	//	}
+	//}
+	//else cout << "unable to open key file!";
+	//bio2.clear();
+
+	//key.close();
 
 
 	//////////////////////////////PART FOUR//////////////////////////////////////////////////////////////////////////////////
 	//////////////////////////////XOR operation and save the file into out////////////////////////////////////////
+	char *Encrypted_message;
+	Encrypted_message = new char[length];
+
+	bitset<16> first,result;						///////////////////////fist bitset is from key 
+			
+	for (int i = 0; i < length; i++)
+	{
+		first = bio.pop_front();					//////////////////////Get a bitset
+		bitset<16> second(message[i]);				///////////////////////second bitset from text (message)
+	//	cout << first << " " << second <<" "<< message[i]<< endl;
+		result = first ^= second;														/////////////XOR operation
+		unsigned long x=result.to_ulong();
+		Encrypted_message[i] = static_cast<char>(x);									//////transform bitset into char
+	//	cout << "XOR result in binary: "<<result<<", XOR result as text: "<< Encrypted_message[i] << endl;
+	//	cout << "haha!" << endl;
+	}
+
+	ofstream outfile(filename3, ios::out);
+	for (int i = 0; i < length; i++)
+		outfile << Encrypted_message[i];
+	outfile.close();
 
 
-	///code part////
 
+	///////////////////////////PART FIVE////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////decryption from the Encrypted_message and the key file from PART THREE //////////////////////
+	
+	
+	///code part
 
 
 	return 0;
